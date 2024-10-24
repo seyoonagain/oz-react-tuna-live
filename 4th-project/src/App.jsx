04d8a21b53ecf430cc6d39cebaf2d94feaff68b4
Seyoon Jeong <seyoonagain@gmail.com>
2024-10-24 20:50:10 +0900

    fourth project

--- 4th-project/src/App.jsx
+++ 4th-project/src/App.jsx
@@ -22,19 +22,25 @@ function App() {
   return (
     <>
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>count + 1</button>
+      <button onClick={() => setCount(count + 1)}>
+        count + 1 하면서 count값이 바뀌니까 useMemo 계속 실행 + 리렌더링 되면서
+        countRef 값도 같이 렌더링
+      </button>
       <p>useMemo로 count까지의 합 세는 중: {countMemo}</p>
       <button onClick={() => setCountMemo(memo)}>
-        이전 count 상태 업데이트 때 실행한 useMemo에 저장된 값 가져오기
+        이거 누를 땐 useMemo 실행 안하고 이전 count 상태 업데이트 때 실행한
+        useMemo에 저장된 값 가져오기
       </button>
       <p>countRef는 count 상태 업데이트되도 초기값으로 안바뀜: {countRef}</p>
       <button
         onClick={() => {
           ref.current += 1;
-          setCountRef(ref.current);
         }}
       >
         countRef + 1
+      </button>
+      <button onClick={() => setCountRef(ref.current)}>
+        countRef 업데이트 렌더링
       </button>
     </>
   );
